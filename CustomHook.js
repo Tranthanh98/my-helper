54b9067ea2104a8cd99efeaaaf16ea7efc71a27c
Tranthanh98 <51749033+Tranthanh98@users.noreply.github.com>
2021-05-02 09:57:06 +0700

    Add files via upload

---
+++ CustomHook.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { isNumeric } from "./helper";
+
+export const useInputText = (initValue, validate = null, isNumber = false)=>{
+    const [value, setValue] = useState(initValue);
+    const [err, setErr] = useState({
+        error: false,
+        helperText : null
+    });
+    function onChange(e){
+        var val = e.target.value;
+        
+        if(validate){
+            if(isNumber){
+                if(isNumeric(val)){
+                    setValue(val);
+                }
+            }
+            else{
+                setValue(val);
+            }
+            validate.validate(val)
+                .then(v => {
+                    
+                    setErr({error: false, helperText: null})
+                })
+            .catch(e => {
+                setErr({error: true, helperText: String(e.errors)});
+            });
+        }
+        else{
+            if(isNumber){
+                if(isNumeric(val)){
+                    setValue(val);
+                }
+            }
+            else{
+                setValue(e.target.value);
+            }
+        }
+    }
+    return {
+        value,
+        onChange,
+        ...err,
+        setValue
+    }
+}
